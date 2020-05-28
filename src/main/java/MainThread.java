f3ea918b21272fbb4f5753160c789ec204d96673
Asus <mamuk-kostanyan@mail.ru>
2020-05-28 11:40:09 +0400

    Update Ball.java

--- src/main/java/MainThread.java
+++ src/main/java/MainThread.java
@@ -8,7 +8,7 @@ public class MainThread {
     public static void main(String[] args) {
 
         Application app = new BallApplication();
-//        app = new SquareApplication();
+        app = new SquareApplication();
 //        app = new StarApplication();
         app.run();
     }
